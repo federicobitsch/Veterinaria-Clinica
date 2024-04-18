9c2ee3b75462dd50765fc4c49faf5c3ab4b83d68
Federico <fedegamer11@gmail.com>
2024-04-17 21:06:40 -0300

    Actualizacion del proyecto

--- doc/documentos.py
+++ doc/documentos.py
@@ -1,2 +1,18 @@
 #DOCUMENTOS
 
+class Documents():
+    def __init__(self,nombre,fecha,tarea):
+        self.nombre = nombre
+        self.fecha = fecha
+        self.tarea = tarea
+
+    def agregar_tarea(self,nombre):
+        print("""
+            |=====================|
+            |Vamos a agregar tarea|
+            |=====================|
+            """)
+        
+        opcion = input('¿Que cosas podria mejorar de la web?')
+        
+        
